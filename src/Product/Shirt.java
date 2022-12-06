17ec6b41ee4195e2d6730b80e612969ba0123d84
leanhtuan98 <leanhtuan22091998@gmail.com>
2022-12-06 08:31:24 +0700

    up

---
+++ src/Product/Shirt.java
@@ -0,0 +1,119 @@
+package Product;
+
+import java.io.Serializable;
+
+public class Shirt implements Serializable {
+    public static int INDEX = Integer.valueOf(0);
+    private int id;
+    private String name;
+    private Double price;
+    private int quantity;
+    private Size size;
+    private Brand brand;
+
+
+    public Shirt() {
+    }
+
+
+    public Shirt(String name, Double price, int quantity, Size size, Brand brand) {
+        this.id = Integer.valueOf(++INDEX);
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.size = size;
+        this.brand = brand;
+    }
+
+    public static int getINDEX() {
+        return INDEX;
+    }
+
+    public static void setINDEX(int INDEX) {
+        Shirt.INDEX = INDEX;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price * quantity;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        if (size != null && brand != null) {
+            return "Shirt{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", price=" + price +
+                    ", quantity=" + quantity +
+                    ", size=" + size.getSize() +
+                    ", brand=" + brand.getName() +
+                    '}';
+        } else {
+            return "Shirt{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", price=" + price +
+                    ", quantity=" + quantity +
+                    ", size  = nothing" +
+                    ", brand = nothing" +
+                    '}';
+
+        }
+    }
+
+    public void show() {
+        if (size != null && brand != null) {
+            System.out.printf("%-10s%-20s%-15s%-15s%-20s%s", id, name, price, quantity, size.getSize(), brand.getName() + "\n");
+        } else {
+            System.out.printf("%-10s%-20s%-15s%-15s%-20s%s", id, name, price, quantity, "nothing", "nothing" + "\n");
+
+        }
+
+    }
+}
+
+
